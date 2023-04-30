2c1077b3199bd319e0df3ee3255f6cc3fe49a269
Kayky <125319686+Kayykky@users.noreply.github.com>
2023-04-30 11:39:19 -0300

    Update protect.php

--- biblioteca/protect.php
+++ biblioteca/protect.php
@@ -1,12 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br" >
+<head>
+  <meta charset="UTF-8">
+  <title>Acesso negado</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="icon" type="image/x-icon" href="./assets/favicon.ico">
+</head>
+<body>
+<div class="wrapper">
+    <fieldset>
+        <?php
+            if(!isset($_SESSION)) {
+                session_start();
+            }
 
-if(!isset($_SESSION)) {
-    session_start();
-}
+            if(!isset($_SESSION['id'])) {
+                echo "<div class='protect'>Você não pode acessar esta página porque não está logado. <br><br>
+                <button onclick=\"location.href='index.php'\" class='button3'>Entrar</button></div>";
+                exit(); 
+            }
+        ?>
+    </fieldset>
+</div>
 
-if(!isset($_SESSION['id'])) {
-    die("Você não pode acessar esta página porque não está logado.<p><a href=\"index.php\">Entrar</a></p>");
-}
-
-
-?>
+</body>
+</html>
