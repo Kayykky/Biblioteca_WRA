a70144ad82ccb751e14511518d070d3e99b7216f
Kayky <125319686+Kayykky@users.noreply.github.com>
2023-05-05 15:13:05 -0300

    Update biblioteca.sql

--- biblioteca.sql
+++ biblioteca.sql
@@ -18,7 +18,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `biblioteca`
+CREATE DATABASE `biblioteca`;
+USE `biblioteca`;
 --
 
 -- --------------------------------------------------------
